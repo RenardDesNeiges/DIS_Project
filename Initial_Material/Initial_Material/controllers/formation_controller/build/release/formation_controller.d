1f7831322604043475869d2a8e124098d99adcad
Titouan Renard <titouan.renard@gmail.com>
2021-04-29 13:39:40 +0200

    leader_follower

--- Initial_Material/Initial_Material/controllers/formation_controller/build/release/formation_controller.d
+++
@@ -1,7 +0,0 @@
-build/release/formation_controller.o: formation_controller.c \
-  /Applications/Webots.app/include/controller/c/webots/robot.h \
-  /Applications/Webots.app/include/controller/c/webots/types.h \
-  /Applications/Webots.app/include/controller/c/webots/nodes.h \
-  /Applications/Webots.app/include/controller/c/webots/differential_wheels.h \
-  /Applications/Webots.app/include/controller/c/webots/distance_sensor.h \
-  /Applications/Webots.app/include/controller/c/webots/motor.h
