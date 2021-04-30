8f4983825a120d0f042f1fb5f7b518422ae73073
Titouan Renard <titouan.renard@gmail.com>
2021-04-30 11:24:46 +0200

    leader with breitenberg and odometry

---
+++ Initial_Material/Initial_Material/controllers/leader/build/release/trajectories.d
@@ -0,0 +1,5 @@
+build/release/trajectories.o: trajectories.c \
+  /Applications/Webots.app/include/controller/c/webots/robot.h \
+  /Applications/Webots.app/include/controller/c/webots/types.h \
+  /Applications/Webots.app/include/controller/c/webots/nodes.h \
+  /Applications/Webots.app/include/controller/c/webots/motor.h
